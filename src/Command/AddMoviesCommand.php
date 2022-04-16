df50bcb30f5d20da0ebb9e725a4c3e831386f87d
Mad10rd <josedavid.lopez.buendia@outlook.es>
2022-04-17 01:55:45 +0200

    Parte 2: Refactorizacion del codigo y agregada una progress bar en el comando

--- src/Command/AddMoviesCommand.php
+++ src/Command/AddMoviesCommand.php
@@ -23,6 +23,8 @@ use App\Entity\Producer;
 
 use Doctrine\Persistence\ManagerRegistry;
 
+use Symfony\Component\Console\Helper\ProgressBar;
+
 #[AsCommand(
     name: 'add-movies',
     description: 'Import movies from CSV file',
@@ -42,8 +44,7 @@ class AddMoviesCommand extends Command
 
     protected function configure(): void
     {
-        $this
-            ->addArgument('csv_route', InputArgument::REQUIRED, 'Route of the CSV file')
+        $this->addArgument('csv_route', InputArgument::REQUIRED, 'Route of the CSV file')
         ;
     }
 
@@ -57,77 +58,86 @@ class AddMoviesCommand extends Command
         $moviesCsv = $decoder->decode(file_get_contents($csvRoute), 'csv');
 
         $em = $this->entityManager;
+        //Barra de progreso para poder controlar en que punto de la importación nos encontramos
+        $progressBar = new ProgressBar($output, count($moviesCsv));
+        $progressBar->start();
 
         foreach($moviesCsv as $movieCsv){
             try{
-                $movie = new Movies();
+                //Si ya existe la pelicula en la base de datos, saltamos a la siguiente
+                if(!$existingProducer = $em->getRepository(Movies::class)->findOneBy(['title' => $movieCsv['title']])) {
+                    $movie = new Movies();
 
-                //Fecha de publicación formateada
-                $date = new \DateTime($movieCsv['date_published']);
-                //Insertamos los datos base de la pelicula
-                $movie->setTitle($movieCsv['title'])
-                      ->setPublicationDate($date)
-                      ->setDuration($movieCsv['duration']);
-                
-                //Agregamos la productora -- En caso de no existir previamente en base de datos, la creamos
-                if($existingProducer = $em->getRepository(Producer::class)->findOneBy(['name' => $movieCsv['production_company']])){
-                    $movie->setProducer($existingProducer);
-                }else{
-                    $producer = new Producer();
-                    $producer->setName($movieCsv['production_company']);
-                    $em->persist($producer);
-                    $movie->setProducer($producer);
-                }
-                //Agregamos genero(s) -- En caso de no existir previamente en base de datos, lo creamos
-                $genresCsv = explode(",", $movieCsv['genre']);
-                foreach($genresCsv as $genreCsv){
-                    if($existingGenre = $em->getRepository(Genre::class)->findOneBy(['name' => $genreCsv])){
-                        $movie->addGenre($existingGenre);
+                    // Fecha de publicación formateada
+                    $date = new \DateTime($movieCsv['date_published']);
+                    // Insertamos los datos base de la pelicula
+                    $movie->setTitle($movieCsv['title'])
+                        ->setPublicationDate($date)
+                        ->setDuration($movieCsv['duration'])
+                    ;
+
+                    // Agregamos la productora -- En caso de no existir previamente en base de datos, la creamos
+                    if($existingProducer = $em->getRepository(Producer::class)->findOneBy(['name' => $movieCsv['production_company']])) {
+                        $movie->setProducer($existingProducer);
                     }else{
-                        $genre = new Genre();
-                        $genre->setName($genreCsv);
-                        $em->persist($genre);
-                        $movie->addGenre($genre);
+                        $producer = new Producer();
+                        $producer->setName($movieCsv['production_company']);
+                        $em->persist($producer);
+                        $movie->setProducer($producer);
                     }
-                }
-                
-                //Agregamos actor(es) -- En caso de no existir previamente en base de datos, lo creamos
-                $actorsCsv = explode(",", $movieCsv['actors']);
-                foreach($actorsCsv as $actorCsv){
-                    if($existingActor = $em->getRepository(Actor::class)->findOneBy(['name' => $actorCsv])){
-                        $movie->addActor($existingActor);
-                    }else{
-                        $actor = new Actor();
-                        $actor->setName($actorCsv);
-                        $em->persist($actor);
-                        $movie->addActor($actor);
+                    // Agregamos genero(s) -- En caso de no existir previamente en base de datos, lo creamos
+                    $genresCsv = explode(',', $movieCsv['genre']);
+                    foreach($genresCsv as $genreCsv) {
+                        if ($existingGenre = $em->getRepository(Genre::class)->findOneBy(['name' => $genreCsv])) {
+                            $movie->addGenre($existingGenre);
+                        } else {
+                            $genre = new Genre();
+                            $genre->setName($genreCsv);
+                            $em->persist($genre);
+                            $movie->addGenre($genre);
+                        }
                     }
-                }
-                
-                //Agregamos director(es) -- En caso de no existir previamente en base de datos, lo creamos
-                $directorsCsv = explode(",", $movieCsv['director']);
-                foreach($directorsCsv as $directorCsv){
-                    if($existingDirector = $em->getRepository(Director::class)->findOneBy(['name' => $directorCsv])){
-                        $movie->addDirector($existingDirector);
-                    }else{
-                        $director = new Director();
-                        $director->setName($directorCsv);
-                        $em->persist($director);
-                        $movie->addDirector($director);
+
+                    // Agregamos actor(es) -- En caso de no existir previamente en base de datos, lo creamos
+                    $actorsCsv = explode(',', $movieCsv['actors']);
+                    foreach($actorsCsv as $actorCsv) {
+                        if($existingActor = $em->getRepository(Actor::class)->findOneBy(['name' => $actorCsv])) {
+                            $movie->addActor($existingActor);
+                        }else{
+                            $actor = new Actor();
+                            $actor->setName($actorCsv);
+                            $em->persist($actor);
+                            $movie->addActor($actor);
+                        }
                     }
-                }
-                
-                $em->persist($movie);
-                $em->flush();
+
+                    // Agregamos director(es) -- En caso de no existir previamente en base de datos, lo creamos
+                    $directorsCsv = explode(',', $movieCsv['director']);
+                    foreach($directorsCsv as $directorCsv) {
+                        if($existingDirector = $em->getRepository(Director::class)->findOneBy(['name' => $directorCsv])) {
+                            $movie->addDirector($existingDirector);
+                        }else{
+                            $director = new Director();
+                            $director->setName($directorCsv);
+                            $em->persist($director);
+                            $movie->addDirector($director);
+                        }
+                    }
+
+                    $em->persist($movie);
+                    $em->flush();
+                }                
+
             }catch(\Exception $e){
                 $output->writeln('La pelicula con el siguiente id no se insertará en la base de datos: '.$movieCsv['imdb_title_id'].' debido a: '.$e->getMessage());
+                //Reseteamos el entityManager porque se cierra automáticamente cuando entramos a la excepción
                 $this->managerRegistry->resetManager();
             }
+            $progressBar->advance();
         }
-        
+        $progressBar->finish();
         $io->success('End!');
 
         return Command::SUCCESS;
     }
-
 }
